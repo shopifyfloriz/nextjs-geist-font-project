d63eb7941c2f2c5b81733efe1e2ae4449a2296a3
shopifyfloriz <shopifyfloriz@gmail.com>
2025-05-15 01:11:40 +0500

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1747253498326

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_next_dist_05476590._.js",
+  "static/chunks/src_ee2d32d2._.js"
+],
+    source: "dynamic"
+});
